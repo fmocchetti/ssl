0a9b8f05fca248afdbf73d7d7e2f95d85295efa3
Federico Mocchetti <fmocchetti@garrahan.gov.ar>
2017-03-27 12:42:31 -0300

    TP 0

---
+++ HelloWorldC/README.md
@@ -0,0 +1,11 @@
+TP 0 - SSL
+
+Primer programa en C, imprimir un hello world!
+
+Compilar:
+
+> gcc hello.c -o hello
+
+Ejecutar:
+
+> ./hello > output.txt
---
+++ HelloWorldC/hello.c
@@ -0,0 +1,11 @@
+/*	Author: Federico Adolfo Mocchetti - Legajo: 1408306
+ *	Catedra: SSL K2051 - Lunes Noche
+ *	Tp 0: Imprimir un hello world en pantalla y atrapar la salida
+**/
+
+#include <stdio.h>
+
+int main(void) {
+	printf('Hello, World!\n');
+	return 0;
+}
